c2e2c0cf964c083c34d9170a60c95deb73b08453
Stephen M Abbott <stephenabbott20@gmail.com>
2025-07-31 18:43:02 -0700

    add for localhost exposure to live and cached status

--- cmd/oss-deps-explorer/main.go
+++ cmd/oss-deps-explorer/main.go
@@ -37,6 +37,7 @@ import (
 func withCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Expose-Headers", "X-Cache-Status")
 		if r.Method == http.MethodOptions {
 			w.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS")
 			w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
